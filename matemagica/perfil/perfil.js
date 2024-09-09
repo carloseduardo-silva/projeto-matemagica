6d44b2edb9cc9bd1185ae8e05271ef1e21759575
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2024-09-09 16:20:17 -0300

    Password Toggle

--- matemagica/perfil/perfil.js
+++ matemagica/perfil/perfil.js
@@ -1,12 +1,24 @@
 const passwordInput = document.getElementById('senha');
 const togglePasswordBtn = document.getElementById('toggle-password-btn');
+const newPasswordInput = document.getElementById('nova-senha');
+const toggleNewPasswordBtn = document.getElementById('toggle-newPassword-btn');
 
-toggleButton.addEventListener('click', () => {
+togglePasswordBtn.addEventListener('click', () => {
   if (passwordInput.type === 'password') {
     passwordInput.type = 'text';
     console.log('click')
   } else {
     passwordInput.type = 'password';
+    console.log('click')
+  }
+});
+
+toggleNewPasswordBtn.addEventListener('click', () => {
+  if (newPasswordInput.type === 'password') {
+    passwordInput.type = 'text';
+    console.log('click')
+  } else {
+    newPasswordInput.type = 'password';
     console.log('click')
   }
 });
